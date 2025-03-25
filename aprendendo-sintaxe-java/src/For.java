efccec6904a533e8f873c768cbed55be19456133
HyagoX <hyagojmaria@gmail.com>
2025-03-25 08:37:49 -0300

    Adicionando aulas sobre aulas de repeticao

---
+++ aprendendo-sintaxe-java/src/For.java
@@ -0,0 +1,8 @@
+public class For {
+    public static void main(String[] args) {
+        String cesta[] = { "MAÇA", "MORANGO", "BANANA", "UVA", "CAQUI" };
+
+        for (int i = 0; i <= cesta.length; i++)
+            System.out.println(cesta[i]);
+    }
+}
